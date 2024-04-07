df5b9d616ea7b0290a9e0961468dead20a466abf
Z3NGHACK <148023899+Z3NGHACK@users.noreply.github.com>
2024-04-07 23:26:22 +0700

    Update SceneController.java

--- expense/src/application/SceneController.java
+++ expense/src/application/SceneController.java
@@ -2,16 +2,19 @@ package application;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class SceneController {
 	
@@ -24,15 +27,46 @@ public class SceneController {
 	private Label get_from_add, get_from_edit;
 	
 	@FXML
-	public void login(ActionEvent event) throws IOException{
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("log.fxml"));
-		Parent root = loader.load();
-		
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
+	private TextField set_input_add, set_input_edit;
+	
+	@FXML
+    private TextField usernameField, passwordField;
+	
+	//singup element
+	@FXML
+    private TextField email, user, phone, cpass, rpass;	
+	
+	@FXML
+	private Label alert_message, wname, wbalance, wemail, wphone, wpass;
+	
+	@FXML
+	private Label sport_input, enter_input, house_input,
+	cloth_input, tran_input, food_input, shop_input, school_input;
+	
+	@FXML
+	public void login(ActionEvent event) throws IOException {
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("log.fxml"));
+	    Parent root = loader.load();
+
+	    Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	    Scene scene = new Scene(root);
+	    stage.setScene(scene);
+	    stage.show();
+	    
 	}
+	
+//	private void checkLogin() throws IOException{
+//		if(usernameField.getText().isEmpty() || passwordField.getText().isEmpty())
+//		{
+//			alert_message.setText("Invalid Input!");
+//			System.out.print("djfvkjdnv");
+//		}
+//	}
+	
+	public boolean isNotEmpty(String text) {
+	    return !text.isEmpty();
+	}
+
 	
 	@FXML
 	public void signup(ActionEvent event) throws IOException{
@@ -47,8 +81,13 @@ public class SceneController {
 	
 	@FXML
 	public void wallet(ActionEvent event) throws IOException{
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("waller_page.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("wallet_page.fxml"));
 		Parent root = loader.load();
+		
+		wname.setText(user.getText());
+		wemail.setText(email.getText());
+		wphone.setText(phone.getText());
+		wpass.setText(cpass.getText());
 		
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
@@ -58,12 +97,61 @@ public class SceneController {
 	
 	@FXML
 	public void main(ActionEvent event) throws IOException{
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("mainPage.fxml"));
-		Parent root = loader.load();
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
+		if(usernameField.getText().isEmpty() || passwordField.getText().isEmpty()){
+			alert_message.setText("Invalid Input!");
+		}
+//		else if(email.getText().isEmpty() || user.getText().isEmpty()
+//				 || phone.getText().isEmpty() || cpass.getText().isEmpty()
+//				 || rpass.getText().isEmpty()){
+//			alert_message.setText("Invalid Input!");
+//		}
+		else {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("mainPage.fxml"));
+			Parent root = loader.load();
+			
+			System.out.print(usernameField.getText());
+			System.out.print(passwordField.getText());
+
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+	}
+	
+	@FXML
+	public void rMain(ActionEvent event) throws IOException{
+		if(email.getText().isEmpty() || user.getText().isEmpty()
+				 || phone.getText().isEmpty() || cpass.getText().isEmpty()
+				 || rpass.getText().isEmpty()){
+			alert_message.setText("Invalid Input!");
+		}
+		else {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("mainPage.fxml"));
+			Parent root = loader.load();
+			
+
+			System.out.print(email.getText());
+			System.out.print(user.getText());
+			System.out.print(phone.getText());
+			System.out.print(cpass.getText());
+			System.out.print(rpass.getText());
+
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+	}
+	@FXML
+	public void dMain(ActionEvent event) throws IOException{
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("mainPage.fxml"));
+			Parent root = loader.load();
+
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
 	}
 	
 	@FXML
@@ -82,11 +170,22 @@ public class SceneController {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("expenseDetail.fxml"));
 		Parent root = loader.load();
 		
+		SceneController controller = loader.getController();
+		
+//		controller.sport_input.setText("0");
+//		controller.enter_input.setText("0");
+//		controller.house_input.setText("0");
+//		controller.cloth_input.setText("0");
+//		controller.tran_input.setText("0");
+//		controller.food_input.setText("0");
+//		controller.shop_input.setText("0");
+//		controller.school_input.setText("0");
 		
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+		
 	}
 	
 	@FXML
@@ -99,35 +198,35 @@ public class SceneController {
 		
 		switch(buttonId) {
 			case "add_sport":
-				controller.setLabelTextAdd("Sport: ");
+				controller.setLabelTextAndInputAdd("Sport: ", "get_input_sport");
 				break;
 				
 			case "add_enter":
-				controller.setLabelTextAdd("Entertainment: ");
+				controller.setLabelTextAndInputAdd("Entertainment: ", "get_input_enter");
 				break;
 				
 			case "add_house":
-				controller.setLabelTextAdd("Housing: ");
+				controller.setLabelTextAndInputAdd("Housing: ", "get_input_house");
 				break;
 				
 			case "add_cloth":
-				controller.setLabelTextAdd("Clothing: ");
+				controller.setLabelTextAndInputAdd("Clothing: ", "get_input_cloth");
 				break;
 				
 			case "add_tran":
-				controller.setLabelTextAdd("Transportation: ");
+				controller.setLabelTextAndInputAdd("Transportation: ", "get_input_tran");
 				break;
 				
 			case "add_food":
-				controller.setLabelTextAdd("Food: ");
+				controller.setLabelTextAndInputAdd("Food: ", "get_input_food");
 				break;
 				
 			case "add_shop":
-				controller.setLabelTextAdd("Shoping: ");
+				controller.setLabelTextAndInputAdd("Shoping: ", "get_input_shop");
 				break;
 				
 			case "add_school":
-				controller.setLabelTextAdd("School fee: ");
+				controller.setLabelTextAndInputAdd("School fee: ", "get_input_school");
 				break;
 			default: 
 				break;
@@ -140,6 +239,18 @@ public class SceneController {
 	}
 	
 	@FXML
+	public void handleAddButton(ActionEvent event) {
+		String inputID = set_input_add.getId();
+		String inputText = set_input_add.getText();
+	}
+	
+	@FXML
+	public void handleEditButton(ActionEvent event) {
+		String inputID = set_input_edit.getId();
+		String inputText = set_input_edit.getText();
+	}
+	
+	@FXML
 	public void edit(ActionEvent event) throws IOException{
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("edit.fxml"));
 		Parent root = loader.load();
@@ -149,35 +260,35 @@ public class SceneController {
 		
 		switch(buttonId) {
 			case "edit_sport":
-				controller.setLabelTextEdit("Sport: ");
+				controller.setLabelTextAndInputEdit("Sport: ", "get_input_sport");
 				break;
 				
 			case "edit_enter":
-				controller.setLabelTextEdit("Entertainment: ");
+				controller.setLabelTextAndInputEdit("Entertainment: ", "get_input_enter");
 				break;
 				
 			case "edit_house":
-				controller.setLabelTextEdit("Housing: ");
+				controller.setLabelTextAndInputEdit("Housing: ", "get_input_house");
 				break;
 				
 			case "edit_cloth":
-				controller.setLabelTextEdit("Clothing: ");
+				controller.setLabelTextAndInputEdit("Clothing: ", "get_input_cloth");
 				break;
 				
 			case "edit_tran":
-				controller.setLabelTextEdit("Transportation: ");
+				controller.setLabelTextAndInputEdit("Transportation: ", "get_input_tran");
 				break;
 				
 			case "edit_food":
-				controller.setLabelTextEdit("Food: ");
+				controller.setLabelTextAndInputEdit("Food: ", "get_input_food");
 				break;
 				
 			case "edit_shop":
-				controller.setLabelTextEdit("Shoping: ");
+				controller.setLabelTextAndInputEdit("Shoping: ", "get_input_shop");
 				break;
 				
 			case "add_school":
-				controller.setLabelTextEdit("School fee: ");
+				controller.setLabelTextAndInputEdit("School fee: ", "get_input_school");
 				break;
 			default: 
 				break;
@@ -189,10 +300,23 @@ public class SceneController {
 		stage.show();
 	}
 	
-	public void setLabelTextAdd(String text) {
+//	public void setLabelExpense(String text){
+//		sport_input.setText(text); 
+//		enter_input.setText(text);
+//		house_input.setText(text);
+//		cloth_input.setText(text);
+//		tran_input.setText(text);
+//		food_input.setText(text);
+//		shop_input.setText(text);
+//		school_input.setText(text);
+//	}
+	
+	public void setLabelTextAndInputAdd(String text, String inputId) {
 		get_from_add.setText(text);
+		set_input_add.setId(inputId);
 	}
-	public void setLabelTextEdit(String text) {
+	public void setLabelTextAndInputEdit(String text, String inputId) {
 		get_from_edit.setText(text);
+		set_input_edit.setId(inputId);
 	}
 }
